c8ff4749bbf8acf691880c258347a21d8f3c8ae2
Luis <lecarcamol@gmail.com>
2022-06-15 14:19:29 -0300

    test: Passed 01 and 02

--- modules/splitLogLines.js
+++ modules/splitLogLines.js
@@ -1,5 +1,16 @@
 const fs = require('fs');
 
 const splitLogLines = function(pathString){
+  let content = fs.readFileSync(pathString, 'utf-8')
 
+  let rawLines = content.split("\n");
+  let logLinesArray = rawLines.map(e => {
+    return e.trim();
+  })
+
+  return logLinesArray
 }
+
+splitLogLines('./test_logs/testLog01.log')
+
+module.exports = splitLogLines
--- tests/splitLogLine.test.js
+++ tests/splitLogLine.test.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
+const splitLogLines = require('../modules/splitLogLines');
 
 describe('Split Log Lines', () => {
 
-  it.todo('01. splits lines for testLog01')
+  it('01. returns array with 10 lines', () => {
+    let pathString = "./test_logs/testLog01.log"
+    let functionReturn = splitLogLines(pathString).length
+    let expectedResult = 10
+
+    expect(functionReturn).toEqual(expectedResult)
+  })
+
+  it('02. parses lines with back slashes', () => {
+    let pathString = "./test_logs/testLog01.log"
+    let functionReturn = splitLogLines(pathString)[4]
+    let expectedResult = "20:34 ClientUserinfoChanged: 2 n\\Isgalamido\\t\\0\\model\\xian/default\\hmodel\\xian/default\\g_redteam\\\\g_blueteam\\\\c1\\4\\c2\\5\\hc\\100\\w\\0\\l\\0\\tt\\0\\tl\\0"
+
+    expect(functionReturn).toEqual(expectedResult)
+  })
 
 })
