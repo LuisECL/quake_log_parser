8311bc8cd1168b4d2d062b17763ed6cc051b79c1
Luis <lecarcamol@gmail.com>
2022-06-15 13:35:23 -0300

    Set up splitLogLines function and test
    Created files splitLogLines.js and splitLogLines.test.js

---
+++ modules/splitLogLines.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+const splitLogLines = function(pathString){
+
+}
---
+++ tests/splitLogLine.test.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+describe('Split Log Lines', () => {
+
+  it('01. splits lines for testLog01', () => {
+    
+  })
+
+})
