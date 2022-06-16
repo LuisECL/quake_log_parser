6dadd9c042a6790b567168f1b58815c457f6cbb3
Luis <lecarcamol@gmail.com>
2022-06-16 19:00:42 -0300

    test: Pass first 2 tests
    01. Ignore lines without respective trigger word, 02. Ignore players who haven't change names

--- modules/clientUpdate.js
+++ modules/clientUpdate.js
@@ -1,5 +1,19 @@
 const clientUpdate = function(logLine, playersObj){
 
+  const triggerWord = logLine[1];
+  const clientID = logLine[2];
+
+  if(triggerWord !== "ClientUserinfoChanged:"){
+    return playersObj
+  }
+
+  const userName = logLine[3].split("\\")[1]
+  console.log(userName)
+
+  if(userName == playersObj[clientID]){
+    return playersObj
+  }
+
 }
 
-module.exports(clientUpdate)
+module.exports = clientUpdate
--- tests/clientUpdate.test.js
+++ tests/clientUpdate.test.js
@@ -1,5 +1,27 @@
 const clientUpdate = require('../modules/clientUpdate')
+const logLines = require('../test_logs/logLines')
 
 describe('clientUpdate reading function', ()=> {
-  
+
+  it("01. Ignores lines w/o triggerWord", ()=> {
+    let logLine = ['20:34', 'ClientConnect:', '3']
+    let playersObj = {'2': "Isgalamido"}
+
+    let functionReturn = clientUpdate(logLine, playersObj)
+    let expectedResult = {'2': "Isgalamido"}
+
+    expect(functionReturn).toEqual(expectedResult)
+  })
+
+  it("02. If player hasn't changed names, return playersObj", ()=> {
+    let logLine = logLines.clientUpdateUser2
+    let playersObj = {'2': "Isgalamido"}
+
+    let functionReturn = clientUpdate(logLine, playersObj)
+    let expectedResult = {'2': "Isgalamido"}
+
+    expect(functionReturn).toEqual(expectedResult)
+  })
+
+
 })
