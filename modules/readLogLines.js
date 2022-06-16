39f6902c3487a10d19fb3fa2e653ed7ccb9bb66b
Luis <lecarcamol@gmail.com>
2022-06-15 22:41:57 -0300

    feat: readLogLines
    Created readLogLines function and test files

---
+++ modules/readLogLines.js
@@ -0,0 +1,5 @@
+const readLogLines = function(logLinesArray, allStartEndPoints) {
+
+}
+
+module.exports = readLogLines
---
+++ tests/readLogLines.test.js
@@ -0,0 +1,7 @@
+const readLogLines = require("../modules/readLogLines");
+
+describe("Read log lines", () => {
+
+  it.todo("01. 1 match, 1 player, 0 kills")
+
+})
