62120b0c35805936ffc3894a212f12ffdaa7089f
Luis <lecarcamol@gmail.com>
2022-06-15 23:03:11 -0300

    test: Provisional test
    Provisional test to verify correct iteration through log lines

--- modules/readLogLines.js
+++ modules/readLogLines.js
@@ -1,5 +1,24 @@
 const readLogLines = function(logLinesArray, allStartEndPoints) {
 
+  // Provisional: variable to store time stamps
+  const timeStampArray = []
+
+  allStartEndPoints.forEach(startEndPair => {
+
+    let startPoint = startEndPair[0]
+    let endPoint = startEndPair[1]
+
+
+    for (let i = startPoint; i <= endPoint; i++){
+      let timeStamp = logLinesArray[i].split(" ")[0]
+      timeStampArray.push(timeStamp)
+    }
+
+  });
+
+  console.log(timeStampArray)
+  return timeStampArray
+
 }
 
 module.exports = readLogLines
--- tests/readLogLines.test.js
+++ tests/readLogLines.test.js
@@ -1,7 +1,28 @@
+const getStartEndPoints = require("../modules/getStartEndPoints");
 const readLogLines = require("../modules/readLogLines");
+const splitLogLines = require("../modules/splitLogLines");
+
 
 describe("Read log lines", () => {
 
-  it.todo("01. 1 match, 1 player, 0 kills")
+  it("01. 1 match, 1 player, 0 kills", ()=> {
+    let logLinesArray = splitLogLines('./test_logs/testLog01.log')
+    let allStartEndPoints = getStartEndPoints(logLinesArray)
+    let functionResult = readLogLines(logLinesArray, allStartEndPoints)
+    let expectedResult = [
+      "0:00",
+      "15:00",
+      "20:34",
+      "20:34",
+      "20:37",
+      "20:37",
+      "20:37",
+      "20:37",
+      "20:37"
+    ]
+
+    expect(functionResult).toEqual(expectedResult)
+
+  })
 
 })
