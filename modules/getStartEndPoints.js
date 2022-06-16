427bbc0a108bf2e29307d1fc343ef2f1debb357b
Luis <lecarcamol@gmail.com>
2022-06-15 22:17:58 -0300

    test: Passed all 4 tests

--- modules/getStartEndPoints.js
+++ modules/getStartEndPoints.js
@@ -1,5 +1,37 @@
 const getStartEndPoints = function(logLinesArray){
 
+  let initGameIndexArray = [];
+
+  for (let i=0; i < logLinesArray.length; i++){
+    let logLine = logLinesArray[i]
+    let triggerWord = logLine.split(" ")[1]
+
+    if (triggerWord == "InitGame:"){
+      console.log(triggerWord)
+      initGameIndexArray.push(i)
+    }
+  };
+
+  let allStartEndPoints = [];
+
+  for (let j = 0; j < initGameIndexArray.length; j++){
+    let startEndPoint = []
+    startEndPoint.push(initGameIndexArray[j])
+
+    if ((j+1) == initGameIndexArray.length){
+      let lastLogLine = (logLinesArray.length - 1)
+      startEndPoint.push(lastLogLine)
+    } else {
+      let lastMatchLine = (initGameIndexArray[j+1]-1)
+      startEndPoint.push(lastMatchLine)
+    }
+
+    allStartEndPoints.push(startEndPoint)
+
+  }
+
+  return allStartEndPoints
+
 }
 
 module.exports = getStartEndPoints
--- tests/getStartEndPoints.test.js
+++ tests/getStartEndPoints.test.js
@@ -1,9 +1,40 @@
+const getStartEndPoints = require("../modules/getStartEndPoints");
 const splitLogLines = require("../modules/splitLogLines");
 
 let logLinesArray = splitLogLines('./qgames.log')
 
 describe('Get start and end points', () => {
 
-  it.todo('01. Returns start and end points of 1 match')
+  it('01. Returns start and end points for 1 match', () => {
+    let testLog01LinesArray = splitLogLines('./test_logs/testLog01.log')
+    let functionReturn = getStartEndPoints(testLog01LinesArray)
+    let expectedReturn = [[1, 9]]
+
+    expect(functionReturn).toEqual(expectedReturn)
+  })
+
+  it('02. Returns start and end points for 2 matches', ()=> {
+    let testLog02LinesArray = splitLogLines('./test_logs/testLog02.log')
+    let functionReturn = getStartEndPoints(testLog02LinesArray)
+    let expectedReturn = [[1, 9], [10, 97]]
+
+    expect(functionReturn).toEqual(expectedReturn)
+  })
+
+  it("03. Test 02 + Ignores user with name 'InitGame:' (lines 14 and 19 of testLog02", ()=> {
+    let testLog02LinesArray = splitLogLines('./test_logs/testLog02.log')
+    let functionReturn = getStartEndPoints(testLog02LinesArray)
+    let expectedReturn = [[1, 9], [10, 97]]
+
+    expect(functionReturn).toEqual(expectedReturn)
+  })
+
+  it("04. Test with qgames.log", ()=> {
+    let testLog02LinesArray = splitLogLines('./qgames.log')
+    let functionReturn = getStartEndPoints(testLog02LinesArray)
+    let expectedReturn = [[1, 9], [10, 96], [97, 157], [158, 673], [674, 815], [816, 1011], [1012, 1611], [1612, 1990], [1991, 2354], [2355, 2525], [2526, 2691], [2692, 3405], [3406, 3460], [3461, 4014], [4015, 4063], [4064, 4092], [4093, 4227], [4228, 4295], [4296, 4711], [4712, 4757], [4758, 5305]]
+
+    expect(functionReturn).toEqual(expectedReturn)
+  })
 
 })
