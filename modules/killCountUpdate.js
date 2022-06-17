b86e60001abe28b1d0d68380fb7fffdc10959318
Luis <lecarcamol@gmail.com>
2022-06-16 21:15:27 -0300

    test: Pass all 6 tests
    01. Ignores lines without trigger word, 02. 03. and 04. are world kill scenarios, 05. Add 1 to player's and 1 to total count, 06. Players first kill

--- modules/killCountUpdate.js
+++ modules/killCountUpdate.js
@@ -1,8 +1,32 @@
 const killCountUpdate = function(logLine, killsObj){
 
   let triggerWord = logLine[1];
+  let killerID = logLine[2];
+  let deathID = logLine[3]
+  let worldID = "1022"
 
   if(triggerWord != "Kill:"){
+    return killsObj
+  }
+
+  if(killerID == worldID){
+    killsObj.total++
+
+    if(killsObj[deathID] == 0 || killsObj[deathID] == undefined){
+      return killsObj
+    }
+
+    killsObj[deathID]--
+    return killsObj
+  }
+
+  if(killsObj.hasOwnProperty([killerID])){
+    killsObj[killerID]++
+    killsObj.total++
+    return killsObj
+  } else {
+    killsObj[killerID] = 1
+    killsObj.total++
     return killsObj
   }
 
--- tests/killCountUpdate.test.js
+++ tests/killCountUpdate.test.js
@@ -13,4 +13,54 @@ describe("killCountUpdate function", ()=> {
     expect(functionReturn).toEqual(expectedResult)
   })
 
+  it("02. World kill: Don't subtract if player's count is 0, but add 1 to total kills", ()=> {
+    let logLine = logLines.worldKillUser2
+    let killsObj = {total: 0, '2': 0}
+
+    let functionReturn = killCountUpdate(logLine, killsObj)
+    let expectedResult = {total: 1, '2':0}
+
+    expect(functionReturn).toEqual(expectedResult)
+  })
+
+  it("03. World kill: Don't subtract if player isn't in killsObj, but add 1 to total kills", ()=> {
+    let logLine = logLines.worldKillUser2
+    let killsObj = {total: 0}
+
+    let functionReturn = killCountUpdate(logLine, killsObj)
+    let expectedResult = {total: 1}
+
+    expect(functionReturn).toEqual(expectedResult)
+  })
+
+  it("04. World kill: Subtract 1 from players count and add 1 to total kills", ()=> {
+    let logLine = logLines.worldKillUser2
+    let killsObj = {total: 1, '2':1}
+
+    let functionReturn = killCountUpdate(logLine, killsObj)
+    let expectedResult = {total: 2, '2':0}
+
+    expect(functionReturn).toEqual(expectedResult)
+  })
+
+  it("05. Adds 1 to player's kill count and total kills", ()=> {
+    let logLine = logLines.user2KillUser3
+    let killsObj = {total: 1, '2':1}
+
+    let functionReturn = killCountUpdate(logLine, killsObj)
+    let expectedResult = {total: 2, '2':2}
+
+    expect(functionReturn).toEqual(expectedResult)
+  })
+
+  it("06. Players first kill: start players count at 1 and add 1 to total kills", ()=> {
+    let logLine = logLines.user2KillUser3
+    let killsObj = {total: 1}
+
+    let functionReturn = killCountUpdate(logLine, killsObj)
+    let expectedResult = {total: 2, '2':1}
+
+    expect(functionReturn).toEqual(expectedResult)
+  })
+
 })
